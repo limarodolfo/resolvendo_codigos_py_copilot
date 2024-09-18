dad7bad4feaf415e1c347bfb29b6382d0ccda087
Rodolfo Lima <130995995+limarodolfo@users.noreply.github.com>
2024-09-18 03:02:16 +0000

    Atualização_op_mat.py

--- op_mat.py
+++ op_mat.py
@@ -5,13 +5,13 @@ num2 = int(input("Digite o segundo número: "))
 
 operacao = input("Digite a operação que deseja realizar (+, -, *, /): ")
 
-if = operacao == '+':
+if operacao == '+':
     print(num1 + num2)
-elif = operacao == '-':
+elif operacao == '-':
     print(abs(num1 - num2))
-elif = operacao == '*':
+elif operacao == '*':
     print(num1 * num2)
-elif = operacao == '/':
+elif operacao == '/':
     print(num1 / num2)
 else: 
     print ("Operação inválida")
